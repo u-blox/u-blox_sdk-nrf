14eb8df5f0c231aa2301a1dff328026f6cb3d3b9
Sebastian Cabrera <sebastian.cabrera@u-blox.com>
2026-03-23 20:16:28 +0100

    ras_rreq: set volatile flag for real-time data subscription and refactor callback assignment

--- subsys/bluetooth/services/ras/rreq/ras_rreq.c
+++ subsys/bluetooth/services/ras/rreq/ras_rreq.c
@@ -985,11 +985,11 @@ int bt_ras_rreq_realtime_rd_subscribe(struct bt_conn *conn, struct net_buf_simpl
 		return err;
 	}
 
-	if (!err) {
-		rreq->real_time_rd.data_cb = data_received_cb;
-		rreq->real_time_rd.ranging_data_out = ranging_data_out;
-		net_buf_simple_reset(ranging_data_out);
-	}
+	/*ubx patch start*/
+	rreq->real_time_rd.data_cb = data_received_cb;
+	rreq->real_time_rd.ranging_data_out = ranging_data_out;
+	net_buf_simple_reset(ranging_data_out);
+	/*ubx patch end*/
 
 	return 0;
 }
